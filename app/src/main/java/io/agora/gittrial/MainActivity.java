a50b5348edfc34df30725d09b872c143a09a8e11
Faizan Ali <zaan9823@gmail.com>
2023-04-12 06:56:25 +0530

    Final merge change by faizan

--- app/src/main/java/io/agora/gittrial/MainActivity.java
+++ app/src/main/java/io/agora/gittrial/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Changes 1 by faizan only");
+        System.out.println("Changes 1 and 3 by faizan only");
         System.out.println("Changes made by the master branch");
         System.out.println("This line will be merge");
     }
