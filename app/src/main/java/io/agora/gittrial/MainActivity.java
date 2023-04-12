7a77f69e6ee74d84d4e73f53eec7d44626136c1a
Faizan Ali <zaan9823@gmail.com>
2023-04-12 06:38:01 +0530

    Change 1 by master itself

--- app/src/main/java/io/agora/gittrial/MainActivity.java
+++ app/src/main/java/io/agora/gittrial/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Changes 1 by faizan");
+        System.out.println("Changes 1 by faizan only");
         System.out.println("Changes made by the master branch");
     }
 }
